9e19dc0c07b87d117addd3edfd98f7e2e7cb4d85
cgonzalezg <cesargonz1984@gmail.com>
2015-11-14 11:42:44 +0100

    add plantillas mapping

--- server.js
+++ server.js
@@ -3,7 +3,30 @@
  */
 var Indexer = require('./lib/indexer.js');
 var matchesFile = './data_matches/resultados.csv';
+var elasticsearch = require('elasticsearch');
 
-Indexer.matches(matchesFile, function () {
-  console.log('end');
+var index = 'liga';
+
+var client = new elasticsearch.Client({
+  host: 'boot2docker.me:9200',
+});
+client.indices.delete({
+  index: index
+}, function(err, resp) {
+  console.log('DELETE');
+  console.log(resp);
+  console.log(err);
+  console.log(resp);
+  client.indices.create({
+    index: index
+  }, function(err, resp) {
+    console.log('CREATE');
+    console.log(err);
+    console.log(resp);
+    Indexer.matches(matchesFile, index, function() {
+      Indexer.plantillas('./data_matches/platillas.csv', index, function() {
+        console.log('finish');
+      });
+    });
+  });
 });
