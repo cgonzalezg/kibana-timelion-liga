a71df3c38993bef3d7cba98c2bb29c7e3ae32bb6
cgonzalezg <cesargonz1984@gmail.com>
2015-11-14 14:09:59 +0100

    refactor code

--- server.js
+++ server.js
@@ -4,29 +4,46 @@
 var Indexer = require('./lib/indexer.js');
 var matchesFile = './data_matches/resultados.csv';
 var elasticsearch = require('elasticsearch');
-
+var async = require('async');
 var index = 'liga';
 
 var client = new elasticsearch.Client({
   host: 'boot2docker.me:9200',
 });
-client.indices.delete({
-  index: index
-}, function(err, resp) {
-  console.log('DELETE');
-  console.log(resp);
-  console.log(err);
-  console.log(resp);
-  client.indices.create({
-    index: index
-  }, function(err, resp) {
-    console.log('CREATE');
-    console.log(err);
-    console.log(resp);
-    Indexer.matches(matchesFile, index, function() {
-      Indexer.plantillas('./data_matches/platillas.csv', index, function() {
-        console.log('finish');
-      });
+
+async.series({
+  deletePartidos: function(cb) {
+    console.log('DELETE', index);
+
+    client.indices.delete({
+      index: index
+    }, cb);
+  },
+  deletePlantillas: function(cb) {
+    console.log('DELETE', index + 2);
+
+    client.indices.delete({
+      index: index + 2
+    }, cb);
+  },
+  createPartidos: function(cb) {
+    console.log('CREATE', index);
+
+    client.indices.create({
+      index: index
+    }, cb);
+  },
+  createPlantillas: function(cb) {
+    console.log('CREATE', index + 2);
+
+    client.indices.create({
+      index: index + 2
+    }, cb);
+  }
+}, function(callback) {
+  Indexer.matches(matchesFile, index, function() {
+    Indexer.plantillas('./data_matches/platillas.csv', index + 2, function() {
+      console.log('finish');
     });
   });
 });
